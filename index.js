e4fce7244fb1db27ca51f08472ca2cabfecb7b82
Abhishek Kumar <abhisheksngh0900@gmail.com>
2022-12-18 19:01:10 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,40 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
+const dbUrl = process.env.DB_URL || "mongodb://localhost/instaclone"
+const mongoose = require('mongoose');
+mongoose.connect(dbUrl, () => console.log("Database Connected"))
+const postRoutes = require('./routes/post')
+const cors = require('cors');
+const corsOptions = {
+    origin: 'https://abhi-frontend-work.onrender.com',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+
+app.use(express.urlencoded());
+app.use(bodyParser.json());
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('form.ejs');
+})
+
+
+
+app.use('/post', postRoutes);
+
+app.use('/*', (req, res) => {
+    res.status(404).send('Page Not Found');
+})
+
+
+app.listen(PORT, () => console.log('Server is on PORT', PORT));
---
+++ routes/post.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const Post = require('../models/post');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const { storage } = require('./cloudinary');
+// parse application/x-www-form-urlencoded
+router.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+router.use(bodyParser.json())
+
+
+const parser = multer({ storage: storage });
+
+router.post('/', parser.single('file'), async (req, res) => {
+    const { name, location, description } = req.body;
+    try {
+        let post = await Post.create({
+            name: name,
+            location: location,
+            description: description,
+            PostImage: req.file.path,
+
+        })
+        res.json({
+            status: "Success",
+            post
+        })
+        console.log(post);
+    }
+    catch (err) {
+        res.status(400).json({
+            status: "Fail",
+            message: err.message
+        })
+
+    }
+})
+
+router.get('/', async (req, res) => {
+    try {
+        let posts = await Post.find().sort({ createdAt: -1 });
+        res.json({
+            status: "Success",
+            posts
+        })
+    }
+    catch (err) {
+        res.status(400).json({
+            status: "Fail",
+            message: err.message
+        })
+    }
+})
+
+
+module.exports = router;
